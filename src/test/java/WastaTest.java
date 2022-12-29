f6cb9092840e55b0b009939354f917d188367b36
WastaTariq <wastatar30@gmail.com>
2022-12-29 22:52:20 +0100

    WastaTest addded again

--- src/test/java/WastaTest.java
+++ src/test/java/WastaTest.java
@@ -4,6 +4,7 @@ public class WastaTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("hello guys good bye");
+        System.out.println("I am adding again");
 
     }
 
